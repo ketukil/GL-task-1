b0ea184f5d456ac61ec6a76e364e134d834154b0
Damjan Belavic <damjan.belavic@vuka.hr>
2021-08-24 21:59:23 +0200

    Day 2 - Late nite brain torture

--- src/sequence_detector/detector.c
+++ src/sequence_detector/detector.c
@@ -1,5 +1,5 @@
 /**
- * @file seq_detect.c
+ * @file detector.c
  * @author Damjan Belavic (damjan.belavic@vuka.hr)
  * @brief GlobalLogic Embedded Software Engineer in C / Task 1
  * @version 1.0
@@ -9,74 +9,53 @@
  * 
  */
 
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <event2/event.h>
-#include "seq_detect.h"
+#include "detector.h"
 
 // ┌───────────────────────────────────────────────────────────────────────────┐
 // │ Enums, Variables, Structures, Typedefs                                    │
 // └───────────────────────────────────────────────────────────────────────────┘
 
-struct event_base *event_base;
-struct event *heartbeat_event, *timeout_event;
-struct timeval time_100ms = {0, 100000};
-struct timeval time_2000ms = {2, 0};
+struct DETECTOR
+{
+    int curr_state;
+    int max_states;
+    const char *sequence;
+    event_t *event;
+    timeval_t *time;
+} detector;
 
-extern uint8_t UART_RX_REG;
 // ┌───────────────────────────────────────────────────────────────────────────┐
 // │ Prototypes                                                                │
 // └───────────────────────────────────────────────────────────────────────────┘
 
-static void Process(evutil_socket_t, short, void *); // Runs every 100ms
 static void Timeout(evutil_socket_t, short, void *); // Timeout after 2000ms
 static void Found();                                 //
-
-// ┌───────────────────────────────────────────────────────────────────────────┐
-// │ Initialization                                                            │
-// └───────────────────────────────────────────────────────────────────────────┘
-
-/**
- * @brief Initialize sequence detector
- * 
- * @param sequence of characters to be detected
- */
-void sequence_detector_init(const char *sequence)
-{
-
-    event_base = event_base_new();
-
-    heartbeat_event = event_new(event_base, -1, EV_PERSIST, &Process, (void *)sequence);
-    timeout_event = event_new(event_base, -1, 0, &Timeout, NULL);
-    event_add(heartbeat_event, &time_100ms);
-
-    event_base_dispatch(event_base);
-}
 
 // ┌───────────────────────────────────────────────────────────────────────────┐
 // │ Business functions                                                        │
 // └───────────────────────────────────────────────────────────────────────────┘
 
+void detector_init(const char *sq, event_t *ev, timeval_t *time)
+{
+    detector.sequence = sq;
+    detector.curr_state = 0;
+    detector.max_states = strlen(sq);
+    detector.event = ev;
+    detector.time = time;
+}
+
 /**
  * @brief Entry point function callled every 100ms
  * 
  */
-static void Process(int fd, short event, void *arg)
+void Process(char symbol)
 {
-    const char *sequence = arg;
     static int index = 0;
-
-    printf("Process (%d, %d, %s): %d\n", fd, event, sequence, index++);
-    if (index == 50)
-        event_add(timeout_event, &time_2000ms);
-
-    if(index == 100)
-        event_add(timeout_event, &time_2000ms); // Rearm oneshot timer
-
-    if (strcmp(sequence, "GLROX") == 0)
-        Found();
+    printf("%c\n", symbol);
 }
 
 /**
@@ -85,7 +64,11 @@ static void Process(int fd, short event, void *arg)
  */
 static void Timeout(int fd, short event, void *arg)
 {
-    printf("Timeout (%d, %d, %p)\n", fd, event, arg);
+    (void)fd;
+    (void)event;
+    (void)arg;
+
+    printf("::: Timeout :::\n");
 }
 
 /**
@@ -94,5 +77,5 @@ static void Timeout(int fd, short event, void *arg)
  */
 static void Found()
 {
-    printf("Found. %c\n", UART_RX_REG);
+    printf("Found.\n");
 }
---
+++ src/sequence_detector/detector.h
@@ -0,0 +1,25 @@
+/**
+ * @file detector.h
+ * @author Damjan Belavic (damjan.belavic@vuka.hr)
+ * @brief GlobalLogic Embedded Software Engineer in C / Task 1
+ * @version 1.0
+ * @date 2021-08-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef DETECTOR_H
+#define DETECTOR_H
+
+#include <stdbool.h>
+#include <event2/event.h>
+
+typedef struct event event_t;
+typedef struct event_base event_base_t;
+typedef struct timeval timeval_t;
+
+void detector_init(const char *, event_t *, timeval_t *);
+void Process(char);
+
+#endif /* DETECTOR_H */
